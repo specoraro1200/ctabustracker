8e9c8f5eed9202b71c2df325653756000e7fb4a6
specoraro1200 <specoraro@luc.edu>
2022-08-27 16:03:10 -0500

    Minor Update: Fix API

--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ import './index.css';
 import App from './App';
 
 import Amplify from 'aws-amplify'
-import config from './aws-exports'
-Amplify.configure(config)
+import awsExports from './aws-exports'
+
+Amplify.configure(awsExports)
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <App />
